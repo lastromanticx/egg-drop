f84e722fcd7ea1c7d29f1d18375f4e720c8cba06
lastromanticx <giladbarkan@live.com>
2016-07-23 08:27:56 -0400

    added comments

--- egg_drop_tabled.rb
+++ egg_drop_tabled.rb
@@ -7,26 +7,28 @@ $m = Array.new(TOP + 1)
 
 (0..TOP + 1).to_a.each do |high|
   $m[high] = Array.new(TOP + 1)
-  
+ 
   (0..TOP + 1).to_a.each do |top|
     $m[high][top] = Array.new(EGGS + 1)
-    
+ 
     (0..EGGS + 1).to_a.each do |eggs|
       $m[high][top][eggs] = Array.new(TOP + 1)
-      
+ 
       (0..TOP + 1).to_a.each do |trials|
-        
-        #
+ 
+        # we cannot solve if we're out of eggs 
+        # before knowing about all the floors
         $m[high][top][0][trials] = Float::INFINITY if high < top - 1
       end
     end
   end
 end
 
-# high: we have a determined answer this and all floors below it
+# high: we have a determined answer for this and all floors below it
 # top: we know the egg will break above this point
 # eggs: how many of them we have left
 # trials: how many we've executed so far
+
 def f high, top, eggs, trials
 
   # if the value is already known, avoid recalculating it
@@ -36,39 +38,47 @@ def f high, top, eggs, trials
 
   $iterations += 1
 
-  # avoid locking the browser in some online IDEs
-  if $iterations > 10000000
-    return Float::INFINITY
-  end
-
-  # we find a possible solution when the highest determined answer 
-  # coincides with the lowest floor above which we know the egg will break
+  # we find a possible solution when we know about all the floors
   if high >= top - 1 && eggs >= 0
     return trials
   end
 
-  # accumulator to update as we try different possibilities
+  # an accumulator to update as we try different possibilities
   best = Float::INFINITY
 
   # since we don't know where to try next, let's try them all
   (high + 1..top).to_a.each do |i|
+
+    # special case: the highest known floor is directly below our
+    # current floor. If the egg were to break here, we would have
+    # our answer; therefore, we pick the longer possibility, with 
+    # more trials, where the egg does not break and our new 'highest-
+    # known floor' is set to i, and minimize on that.
     if high == i - 1
       best = [best, f(i, top, eggs, trials + 1)].min
       next
     end
 
+    # if the highest known floor is not directly below i, breaking
+    # the egg (assuming we have any to break) would determine the 
+    # new higher-bound, meaning the lowest floor above which we have 
+    # an answer for all floors; otherwise, if the egg does not break, we
+    # update the low-bound, the 'highest known floor' again. We pick
+    # the longer of these two possibilities to minimize on.
     best = [best,
       [
-        if eggs > 0 
-           then f(high, i, eggs - 1, trials + 1) 
-           else Float::INFINITY end,
-        f(i, top, eggs, trials + 1)
+       if eggs > 0 
+         then f(high, i, eggs - 1, trials + 1) 
+         else Float::INFINITY end,
+       f(i, top, eggs, trials + 1)
       ].max
     ].min
   end
 
+  # return our accumulator as the best solution for this state of f
+  # and add that value to the matrix
   return $m[high][top][eggs][trials] = best
 end
 
-puts f 0, TOP, EGGS, 0   # 14
-puts $iterations         # 499953
+puts f 0, TOP, EGGS, 0 # 14
+puts $iterations # 499953
