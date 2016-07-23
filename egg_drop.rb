30aa4d1cea3f1d48198f4c04f961d0262f1ba77b
lastromanticx <giladbarkan@live.com>
2016-07-22 21:33:00 -0400

    preliminary, working

---
+++ egg_drop.rb
@@ -0,0 +1,46 @@
+TOP = 100
+EGGS = 2
+$it = 0
+$hash = {}
+
+def f high, top, eggs, trials
+  key = [high,top,eggs,trials].join(",")
+
+  if $hash[key]
+    return $hash[key]
+  end
+
+  $it += 1
+
+  if $it > 10000000
+    return Float::INFINITY
+  end
+
+  if high >= top - 1 && eggs >= 0
+    return trials
+
+  elsif eggs == 0
+    return Float::INFINITY
+  end
+
+  best = Float::INFINITY
+
+  (high + 1..top).to_a.each do |i|
+    if high == i - 1
+      best = [best, f(i,top,eggs,trials + 1)].min
+      next
+    end
+
+    best = [best,
+      [
+        f(high, i, eggs - 1, trials + 1),
+        f(i, top, eggs, trials + 1)
+      ].max
+    ].min
+  end
+
+  return $hash[key] = best
+end
+
+puts f 0, TOP, EGGS, 0   # 14
+puts $it                 # 4587828
